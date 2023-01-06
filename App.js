a77a9ebcdc0b39ed171003334abc0a9e0e5f8c7a
Shivayogi <shivayogihc@gmail.com>
2023-01-06 15:01:50 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
+import StackNavigator from "./StackNavigator";
+import store from "./store";
+
+export default function App() {
+  return (
+    <>
+      <Provider store={store}>
+        <StackNavigator />
+      </Provider>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
